3796ea95ba56e273c448cd54d10129552cc6d2db
Emil Lozev <emilfrogy@gmail.com>
2019-10-16 11:26:20 +0100

    rm fs

--- component---src-pages-page-2-js-5f68fa49defa18b12bcf.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{254:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(5),c=a(251),r=a(250);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-5f68fa49defa18b12bcf.js.map
--- styles-081e1fd60d0affa27c6d.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{209:function(n,o,w){},210:function(n,o,w){}}]);
-//# sourceMappingURL=styles-081e1fd60d0affa27c6d.js.map
