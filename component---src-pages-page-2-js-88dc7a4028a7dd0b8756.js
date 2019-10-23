6f742ad8b0137d1f5e74437e000cec0cbc08152a
Emil Lozev <emilfrogy@gmail.com>
2019-10-23 11:50:00 +0100

    a

---
+++ component---src-pages-page-2-js-88dc7a4028a7dd0b8756.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{263:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(5),c=a(257),r=a(256);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-88dc7a4028a7dd0b8756.js.map
